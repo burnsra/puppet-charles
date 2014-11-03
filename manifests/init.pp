9cabd6cad81df3f33bbe51402986446799504b4d
Robert Burns <burnsra@gmail.com>
2014-11-03 11:04:35 -0500

    Update to version 3.9.3

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,6 +6,6 @@
 class charles {
   package { 'Charles':
     provider => 'appdmg_eula_charles',
-    source   => 'http://www.charlesproxy.com/assets/release/3.9.2/charles-proxy-3.9.2-applejava.dmg',
+    source   => 'http://www.charlesproxy.com/assets/release/3.9.3/charles-proxy-3.9.3-applejava.dmg',
   }
 }
