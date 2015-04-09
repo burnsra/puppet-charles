0838f3941ad8df9677cc0f2727b8dfc8554052c5
Robert Burns <burnsra@gmail.com>
2015-04-09 10:40:45 -0400

    Update to version 3.10.1
    Signed-off-by: Robert Burns <burnsra@gmail.com>

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,6 +6,6 @@
 class charles {
   package { 'Charles':
     provider => 'appdmg_eula_charles',
-    source   => 'http://www.charlesproxy.com/assets/release/3.9.3/charles-proxy-3.9.3-applejava.dmg',
+    source   => 'http://www.charlesproxy.com/assets/release/3.10.1/charles-proxy-3.10.1.dmg',
   }
 }
--- spec/classes/charles_spec.rb
+++ spec/classes/charles_spec.rb
@@ -4,6 +4,6 @@ describe 'charles' do
 
   it { should contain_class('charles') }
   it { should contain_package('Charles').with_provider('appdmg_eula_charles') }
-  it { should contain_package('Charles').with_source('http://www.charlesproxy.com/assets/release/3.9.3/charles-proxy-3.9.3-applejava.dmg') }
+  it { should contain_package('Charles').with_source('http://www.charlesproxy.com/assets/release/3.10.1/charles-proxy-3.10.1.dmg') }
 
 end
